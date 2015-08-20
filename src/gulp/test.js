a6c205b9a8575376f971f74e7fc1cd907a6c920a
jupl <juan.placencia.512@gmail.com>
2015-08-20 12:34:50 -0500

    Use non deprecating Karma API

--- src/gulp/test.js
+++ src/gulp/test.js
@@ -1,18 +1,18 @@
 import extend from 'extend'
 import gulp, {tasks} from 'gulp'
-import karma from 'karma'
+import {Server} from 'karma'
 import rump from 'rump'
 
 const name = ::rump.taskName,
       task = ::gulp.task,
       {configs} = rump
 
-task(name('scripts:test'), () => {
-  const options = extend(true, {}, configs.karma, {
+task(name('scripts:test'), done => {
+  const server = new Server(extend(true, {}, configs.karma, {
     singleRun: !configs.watch,
     webpackServer: {quiet: configs.watch},
-  })
-  karma.server.start(options)
+  }), done)
+  server.start()
 })
 
 tasks[name('test')].dep.push(name('scripts:test'))
